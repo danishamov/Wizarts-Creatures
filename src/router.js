42893b3143b30f0608981459198ecb843f6bb2d5
danishamov <122825234+danishamov@users.noreply.github.com>
2023-11-19 15:57:50 +0000

    add all posts controller to work

--- src/router.js
+++ src/router.js
@@ -2,12 +2,14 @@ const router = require("express").Router();
 
 const homeController = require("./controllers/homeController");
 const userController = require("./controllers/userController");
+const allPostController = require("./controllers/postController");
 
 router.use(homeController);
 router.use("/user", userController);
+router.use("/posts", allPostController);
 
-router.get("*", (req, res) => {
-  res.redirect("/404");
-});
+// router.get("*", (req, res) => {
+//   res.redirect("/404");
+// });
 
 module.exports = router;
